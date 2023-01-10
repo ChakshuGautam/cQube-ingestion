ee757549e7f82bb56a06bdc894b445b7812dd1e9
ChakshuGautam <chaks.gautam@gmail.com>
2023-01-10 14:15:35 +0530

    Fix: Issues with app module importing older module

--- impl/c-qube/src/app.module.ts
+++ impl/c-qube/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SqlQueryBuilderService } from './sql-query-builder/sql-query-builder.service';
 import { QueryBuilderService } from './query-builder/query-builder.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, SqlQueryBuilderService, QueryBuilderService],
+  providers: [AppService, QueryBuilderService],
 })
 export class AppModule {}
