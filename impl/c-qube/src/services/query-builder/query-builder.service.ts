66fad6db0c0e0f0af877446f2659d732a2267d70
Prekshagupta99 <prekshamanglagupta@gmail.com>
2023-01-16 19:10:32 +0530

    update folder structure

--- impl/c-qube/src/services/query-builder/query-builder.service.ts
+++ impl/c-qube/src/services/query-builder/query-builder.service.ts
@@ -4,12 +4,11 @@ import { JSONSchema4 } from 'json-schema';
 export class QueryBuilderService {
   constructor() {}
 
-  generateCreateStatement(schema: JSONSchema4) {
-    const tableName = schema.title;
-    const psqlSchema = schema.psql_schema;
-    let createStatement = `CREATE TABLE ${psqlSchema}.${tableName} (\n`;
+  generateCreateStatement(jsonSchema: JSONSchema4) {
+    const tableName = jsonSchema.title;
+    let createStatement = `CREATE TABLE ${tableName} (\n`;
 
-    const properties = schema.properties;
+    const properties = jsonSchema.properties;
     for (const property in properties) {
       const column: JSONSchema4 = properties[property];
       createStatement += `  ${property} `;
@@ -20,16 +19,15 @@ export class QueryBuilderService {
         (column.format === 'float' || column.format === 'double')
       ) {
         createStatement += 'FLOAT8';
-      } else if (column.type === 'integer') {
-        createStatement += `integer`;
-      } else if (column.type === 'string') {
-        createStatement += `VARCHAR`;
-      } else {
-        console.error('unssuported column.type', column.type);
-        // type not supported
+      }
+      else {
+        createStatement += `${column.type}`;
       }
       if (column.type === 'string' && column.maxLength) {
         createStatement += `(${column.maxLength})`;
+      }
+      if(column.notNull === true){
+        createStatement += ' NOT NULL';
       }
       createStatement += ',\n';
     }
@@ -40,9 +38,8 @@ export class QueryBuilderService {
     return createStatement;
   }
 
-  generateIndexStatement(schema: JSONSchema4): string[] | null {
-    const psqlSchema = schema.psql_schema;
-    const indexStatements = [];
+  generateIndexStatement(schema: JSONSchema4): string | null {
+    let indexStatements = '';
     if (schema.indexes) {
       const indexes = schema.indexes;
 
@@ -50,36 +47,11 @@ export class QueryBuilderService {
         for (const column of index.columns) {
           const indexName = `${schema.title}_${column.join('_')}_idx`;
           const columns = column.join(', ');
-          const statement = `CREATE INDEX ${indexName} ON ${psqlSchema}.${schema.title} (${columns});`;
-          indexStatements.push(statement);
+          const statement = `CREATE INDEX ${indexName} ON ${schema.title} (${columns});`;
+          indexStatements += `${statement}\n`;
         }
       }
     }
     return indexStatements;
-  }
-
-  generateInsertStatement(schema: JSONSchema4, data: any): string {
-    const tableName = schema.title;
-    const psqlSchema = schema.psql_schema;
-    const fields = [];
-    const values = [];
-
-    const propertiesToSkip = ['id'];
-
-    const properties = schema.properties;
-    for (const property in properties) {
-      if (propertiesToSkip.includes(property)) continue;
-      fields.push(property);
-      values.push(`'${data[property]}'`);
-    }
-
-    const query = `INSERT INTO ${psqlSchema}.${tableName} (${fields.join(
-      ', ',
-    )}) VALUES (${values.join(', ')});`;
-    return query;
-  }
-
-  generateUpdateStatement(schema: JSONSchema4, data: any): string[] {
-    throw new Error('Method not implemented.');
   }
 }
