8f36a2bad85eff27c02eb7c77afce51ef21c1f53
ChakshuGautam <chaks.gautam@gmail.com>
2023-01-30 22:09:05 +0530

    Fix: Event Schema

---
+++ impl/c-qube/prisma/migrations/20230130163738_init_12/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `type` on the `EventGrammar` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "spec"."EventGrammar" DROP COLUMN "type";
--- impl/c-qube/src/services/event/event.service.ts
+++ impl/c-qube/src/services/event/event.service.ts
@@ -1,4 +1,111 @@
 import { Injectable } from '@nestjs/common';
+import {
+  InstrumentType,
+  Instrument,
+  Event,
+  EventGrammar,
+} from 'src/types/event';
+import {
+  EventGrammar as EventGrammarModel,
+  InstrumentType as InstrumentTypeModel,
+} from '@prisma/client';
+import { PrismaService } from '../../prisma.service';
+import { QueryBuilderService } from '../query-builder/query-builder.service';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const _ = require('lodash');
 
 @Injectable()
-export class EventService {}
+export class EventService {
+  constructor(
+    public prisma: PrismaService,
+    private qbService: QueryBuilderService,
+  ) {}
+
+  dbModelToEventGrammar(model: EventGrammarModel): EventGrammar {
+    return {
+      name: model.name,
+      instrument: model.instrumentType as unknown as Instrument,
+      description: model.description,
+      schema: model.schema as object,
+      instrument_field: model.instrumentField,
+      is_active: model.isActive,
+    };
+  }
+
+  async createEventGrammar(EventGrammar: EventGrammar): Promise<EventGrammar> {
+    return this.prisma.eventGrammar
+      .create({
+        data: {
+          name: EventGrammar.name,
+          description: EventGrammar.description,
+          schema: EventGrammar.schema,
+          instrumentField: EventGrammar.instrument_field,
+          isActive: EventGrammar.is_active,
+          instrument: {
+            connect: {
+              name: EventGrammar.instrument.name,
+            },
+          },
+        },
+      })
+      .then((model: EventGrammarModel) => this.dbModelToEventGrammar(model));
+  }
+
+  async getEventGrammar(dimensionId: number): Promise<EventGrammar | null> {
+    return this.prisma.eventGrammar
+      .findUnique({
+        where: {
+          id: dimensionId,
+        },
+      })
+      .then((model: EventGrammarModel) => this.dbModelToEventGrammar(model));
+  }
+
+  async getEventGrammarByName(name: string): Promise<EventGrammar | null> {
+    return this.prisma.eventGrammar
+      .findFirst({
+        where: {
+          name: name,
+        },
+      })
+      .then((model: EventGrammarModel) => this.dbModelToEventGrammar(model));
+  }
+
+  async createEvent(
+    EventGrammar: EventGrammar,
+    autoPrimaryKey = true,
+  ): Promise<void> {
+    const createQuery = this.qbService.generateCreateStatement(
+      EventGrammar.schema,
+      autoPrimaryKey,
+    );
+    const indexQuery: string[] = this.qbService.generateIndexStatement(
+      EventGrammar.schema,
+    );
+    await this.prisma.$queryRawUnsafe(createQuery);
+
+    // iterate over indexQuery and execute each query
+    for (const query of indexQuery) {
+      await this.prisma.$queryRawUnsafe(query);
+    }
+  }
+
+  async insertEventData(EventGrammar: EventGrammar, data): Promise<void> {
+    const insertQuery = this.qbService.generateInsertStatement(
+      EventGrammar.schema,
+      data,
+    );
+    return null;
+  }
+
+  async insertBulkEventData(
+    EventGrammar: EventGrammar,
+    data: any[],
+  ): Promise<void> {
+    return null;
+  }
+}
