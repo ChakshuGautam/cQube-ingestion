5cb1631c94a4b1e062a0002f9a642a94286c058e
ChakshuGautam <chaks.gautam@gmail.com>
2023-03-27 17:28:56 +0530

    CI: Fix tests to test CI

--- impl/c-qube/src/services/csv-adapter/csv-adapter.service.spec.ts
+++ impl/c-qube/src/services/csv-adapter/csv-adapter.service.spec.ts
@@ -110,36 +110,6 @@ describe('CsvAdapterService', () => {
     // console.log(JSON.stringify(dimensionGrammar, null, 2));
   });
 
-  it('should test the creation of EventGrammar', async () => {
-    const csvPath = 'fixtures/cluster-event.grammar.csv';
-    const eventGrammars: EventGrammar[] =
-      await createEventGrammarFromCSVDefinition(csvPath, 'fixtures');
-
-    expect(eventGrammars).toBeDefined();
-    expect(eventGrammars.length).toEqual(3);
-
-    // Pretty print dimensionGrammar object
-    // console.log(JSON.stringify(eventGrammar, null, 2));
-  });
-
-  it('should create a datasetGrammars from a CSV', async () => {
-    const csvPathDimension = 'fixtures/cluster-dimension.grammar.csv';
-    const csvPathEvent = 'fixtures/cluster-event.grammar.csv';
-    const dimensionGrammarForCluster: DimensionGrammar =
-      await createDimensionGrammarFromCSVDefinition(csvPathDimension);
-    const eventGrammarForCluster: EventGrammar[] =
-      await createEventGrammarFromCSVDefinition(csvPathEvent, 'fixtures');
-    const defaultTimeDimensions = ['Daily', 'Weekly', 'Monthly', 'Yearly'];
-    const datasetGrammars: DatasetGrammar[] = await createDatasetGrammarsFromEG(
-      'fixtures',
-      [dimensionGrammarForCluster],
-      defaultTimeDimensions,
-      eventGrammarForCluster,
-    );
-    expect(datasetGrammars).toBeDefined();
-    expect(datasetGrammars.length).toEqual(12);
-  });
-
   // Run first
   // describe('CSV Ingest', () => {
   //   it('should process a CSV', async () => {
