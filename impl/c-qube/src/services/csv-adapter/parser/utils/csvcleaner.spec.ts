57c1b9a82d85c00fe93a14a4289c564683643faf
Yash Mittal <mittal.yash12@hotmail.com>
2023-05-07 14:48:32 +0530

    WIP: Fix the last commit:

--- impl/c-qube/src/services/csv-adapter/parser/utils/csvcleaner.spec.ts
+++ impl/c-qube/src/services/csv-adapter/parser/utils/csvcleaner.spec.ts
@@ -1,7 +1,34 @@
-import { readCSV } from './csvreader';
+import {
+  processCsv,
+  removeEmptyLines,
+  getFilesInDirectory,
+} from './csvcleaner';
+import * as fs from 'fs';
 
-describe('CSVCleaner', () => {
-  test('parse the file fine', async () => {});
-  test('throw error', async () => {});
-  test('should throw because of no file found', async () => {});
+describe('remove empty lines', () => {
+  // TODO: Ask what the ReadCSV function does
+  test('parse the file fine', async () => {
+    const testFilePath = './test/fixtures/test-csvs/csvcleaner/withEmpty.csv';
+    const dupFilePath =
+      './test/fixtures/test-csvs/csvcleaner/withEmpty.dup.csv';
+    const withoutEmptyFilePath =
+      './test/fixtures/test-csvs/csvcleaner/withoutEmpty.csv';
+
+    fs.copyFileSync(testFilePath, dupFilePath);
+
+    await removeEmptyLines(dupFilePath);
+    const removedData = fs.readFileSync(dupFilePath, 'utf-8');
+    const withoutData = fs.readFileSync(withoutEmptyFilePath, 'utf-8');
+    expect(removedData).toEqual(withoutData);
+    fs.unlinkSync(dupFilePath);
+  });
+  test('test get files in directory', async () => {
+    const files = await getFilesInDirectory('./test/fixtures/test-csvs');
+    expect(files).toEqual([
+      'test/fixtures/test-csvs/csvcleaner/withEmpty.csv',
+      'test/fixtures/test-csvs/csvcleaner/withoutEmpty.csv',
+      'test/fixtures/test-csvs/csvreader/invalid.reader.csv',
+      'test/fixtures/test-csvs/csvreader/valid.reader.csv',
+    ]);
+  });
 });
