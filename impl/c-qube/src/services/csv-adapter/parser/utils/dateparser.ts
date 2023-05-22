f900a731cf5eb680538b74d0df44a45f34442b9b
Yash Mittal <mittal.yash12@hotmail.com>
2023-05-23 00:57:40 +0530

    fix: update date parser w new implementation

--- impl/c-qube/src/services/csv-adapter/parser/utils/dateparser.ts
+++ impl/c-qube/src/services/csv-adapter/parser/utils/dateparser.ts
@@ -10,31 +10,59 @@ export class DateParser {
     this.timezone = timezone;
   }
 
+  toUtc(date: Date) {
+    const offsetMillis = 5.5 * 60 * 60 * 1000; // 5 hours in milliseconds
+    return new Date(date.getTime() + offsetMillis);
+  }
+
   parseDate(date: string): Date {
-    const parsedDate = parse(date, this.format, new Date());
-    if (this.timezone) {
-      const dateWithTimezone = zonedTimeToUtc(parsedDate, this.timezone);
-      return dateWithTimezone;
-    } else {
-      return new Date(
-        Date.UTC(
-          parsedDate.getFullYear(),
-          parsedDate.getMonth(),
-          parsedDate.getDate(),
-        ),
+    // This assumes date is in the format 'dd-mm-yyyy'
+    if (this.format === 'dd-mm-yyyy' || this.format === 'dd-MM-yyyy') {
+      const parts = date.split('-');
+      const parsedDate = new Date(
+        Number(parts[2]),
+        Number(parts[1]) - 1, // JavaScript months are 0-indexed
+        Number(parts[0]),
       );
-      // const dateWithoutTimezone = new Date(
-      //   new Date(
-      //     parsedDate.getFullYear(),
-      //     parsedDate.getMonth(),
-      //     parsedDate.getDate(),
-      //     'GMT',
-      //   ),
-      // );
-      // dateWithoutTimezone.setDate(dateWithoutTimezone.getDate() + 1);
-      // return dateWithoutTimezone;
+      return this.toUtc(parsedDate);
+    } else if (this.format === 'dd/MM/yy') {
+      const parts = date.split('/');
+      const year = Number(parts[2]);
+      const fullYear = year < 30 ? 2000 + year : 1900 + year;
+      const parsedDate = new Date(
+        fullYear,
+        Number(parts[1]) - 1, // JavaScript months are 0-indexed
+        Number(parts[0]),
+      );
+      return this.toUtc(parsedDate);
     }
   }
+
+  // parseDate(date: string): Date {
+  //   const parsedDate = parse(date, this.format, new Date());
+  //   if (this.timezone) {
+  //     const dateWithTimezone = zonedTimeToUtc(parsedDate, this.timezone);
+  //     return dateWithTimezone;
+  //   } else {
+  //     return new Date(
+  //       Date.UTC(
+  //         parsedDate.getFullYear(),
+  //         parsedDate.getMonth(),
+  //         parsedDate.getDate(),
+  //       ),
+  //     );
+  //     // const dateWithoutTimezone = new Date(
+  //     //   new Date(
+  //     //     parsedDate.getFullYear(),
+  //     //     parsedDate.getMonth(),
+  //     //     parsedDate.getDate(),
+  //     //     'GMT',
+  //     //   ),
+  //     // );
+  //     // dateWithoutTimezone.setDate(dateWithoutTimezone.getDate() + 1);
+  //     // return dateWithoutTimezone;
+  //   }
+  // }
 
   // static getDate(date: Date | string): Date | string {
   //   if (date instanceof Date) {
