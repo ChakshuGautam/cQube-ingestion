b165f0bdd481af3761bc63c44c01b5ccc3e1a6b4
Shashwat_mahajan <shashwatm.cs.21@nitj.ac.in>
2023-08-04 23:47:48 +0530

    final dimension service all covered except one old func

--- impl/c-qube/src/services/dimension/dimension.service.spec.ts
+++ impl/c-qube/src/services/dimension/dimension.service.spec.ts
@@ -3,12 +3,13 @@ import { DimensionGrammar } from 'src/types/dimension';
 import { PrismaService } from '../../prisma.service';
 import { QueryBuilderService } from '../query-builder/query-builder.service';
 import { DimensionService } from './dimension.service';
-import { mockDimensionGrammar } from '../mocks/types.mocks';
+import { grammar } from '../mocks/test.expect';
 
 describe('DimensionService', () => {
   let service: DimensionService;
   let prismaService: PrismaService;
   let qbService: QueryBuilderService;
+  
 
 
   beforeEach(async () => {
@@ -26,22 +27,42 @@ describe('DimensionService', () => {
     expect(service).toBeDefined();
   });
 
+  it('create a new dimension from a DimensionGrammar', async () => {
+    await service.prisma.$executeRawUnsafe(
+      `DROP TABLE IF EXISTS "dimensions"."school" CASCADE;`,
+    );
 
-  it('should insert bulk dimension data and handle any errors', async () => {
-    const dimensionGrammar = mockDimensionGrammar(); 
-    const data = [
-      { name: 'Item 1', id: 1 },
-      { name: 'Item 2', id: 2 },
+    const d = await service.createDimension(grammar, false);
+    const indexes: any[] = await service.prisma.$queryRawUnsafe(
+      `select indexname from pg_indexes where tablename = 'school';`,
+    );
+    const exptectedIndexes = [
+      { indexname: 'school_name_date_created_idx' },
+      { indexname: 'school_name_idx' },
+      { indexname: 'school_date_created_idx' },
     ];
-    prismaService.$queryRawUnsafe = jest.fn().mockImplementation(() => {
-      throw new Error('Some error occurred');
-    });
-    await expect(service.insertBulkDimensionData(dimensionGrammar, data)).rejects.toThrowError('Some error occurred');
-    expect(prismaService.$queryRawUnsafe).toHaveBeenCalledWith(expect.stringContaining('INSERT INTO'));
-
-    
-
+    expect(indexes.sort()).toEqual(exptectedIndexes);
+    await service.prisma.$executeRawUnsafe(
+      `DROP TABLE IF EXISTS "dimensions"."school" CASCADE;`,
+    );
   });
+
+  it('insert dimension data to the school dimenstion table', async () => {
+      await service.prisma.$executeRawUnsafe(
+        `DROP TABLE IF EXISTS "dimensions"."school";`,
+      );
+      await service.createDimension(grammar, false);
+      const data = { name: 'school3', date_created: '2020-01-01T00:00:00.000Z' };
+      await service.insertDimensionData(grammar, data);
+    const result = await service.prisma.$queryRawUnsafe(
+          `select * from dimensions.school;`,
+        );
+        expect(result[0].name).toEqual(data.name);
+        expect(result[0].date_created.toISOString()).toBe(data.date_created);
+        await service.prisma.$executeRawUnsafe(
+          `DROP TABLE IF EXISTS "dimensions"."school";`,
+        );
+      });
 
   it('should return null for an invalid dimension ID', async () => {
     const mockInvalidDimensionId = -1;
@@ -69,117 +90,23 @@ describe('DimensionService', () => {
     });
   });
 
-
-  it('should log errors when there is an error in the create query', async () => {
-    const error = new Error('Some error occurred');
-    const autoPrimaryKey = true;
-    const createQuery = 'CREATE TABLE school ...';
-    const indexQuery = ['CREATE INDEX ...', 'CREATE INDEX ...'];
-    jest.spyOn(qbService, 'generateCreateStatement').mockReturnValue(createQuery);
-    jest.spyOn(qbService, 'generateIndexStatement').mockReturnValue(indexQuery);
-
-    const $queryRawUnsafeSpy = jest.spyOn(prismaService, '$queryRawUnsafe').mockRejectedValue(error);
-    jest.spyOn(console, 'error').mockImplementation();
-    await service.createDimension(mockDimensionGrammar(), autoPrimaryKey);
-
-    expect(qbService.generateCreateStatement).toHaveBeenCalledWith(mockDimensionGrammar().schema, autoPrimaryKey);
-    expect(qbService.generateIndexStatement).toHaveBeenCalledWith(mockDimensionGrammar().schema);
-    expect(prismaService.$queryRawUnsafe).toHaveBeenCalledWith(createQuery);
-    expect(console.error).toHaveBeenCalledWith(mockDimensionGrammar().name);
-    expect(console.error).toHaveBeenCalledWith(JSON.stringify(mockDimensionGrammar(), null, 2));
-    expect(console.error).toHaveBeenCalledWith({ createQuery });
-    expect(console.error).toHaveBeenCalledWith({ indexQuery });
-  });
-
-  it('should insert bulk dimension data and handle errors', async () => {
-    const mockData = [
-       { name: 'Item 1', id: 1 },
-       { name: 'Item 2', id: 2 },
-    ];
-    const mockInsertQuery = 'mock-insert-query';
-    const mockError = new Error('Some error occurred');
-
-    jest.spyOn(qbService, 'generateBulkInsertStatementOld').mockReturnValue(mockInsertQuery);
-    prismaService.$queryRawUnsafe = jest.fn().mockRejectedValue(mockError);
-    console.error = jest.fn(); 
-    console.log = jest.fn(); 
-
-    await service.insertBulkDimensionDataV2(mockDimensionGrammar(), mockData);
-
-    expect(qbService.generateBulkInsertStatementOld).toHaveBeenCalledWith(mockDimensionGrammar().schema, mockData);
-    expect(prismaService.$queryRawUnsafe).toHaveBeenCalledWith(mockInsertQuery);
-    expect(console.error).toHaveBeenCalledWith(mockInsertQuery);
-    if (mockData.length < 50) {
-      expect(console.log).toHaveBeenCalledWith(mockData);
-    }
-    expect(console.error).toHaveBeenCalledWith(mockDimensionGrammar().name);
-    expect(console.error).toHaveBeenCalledWith(mockError);
-  });
-
-  it('should insert bulk dimension data and handle errors', async () => {
-    const mockData = [
-    ];
-    const mockInsertQuery = 'mock-insert-query';
-    const mockError = new Error('Some error occurred');
-
-    jest.spyOn(qbService, 'generateBulkInsertStatementOld').mockReturnValue(mockInsertQuery);
-    prismaService.$queryRawUnsafe = jest.fn().mockRejectedValue(mockError);
-    console.error = jest.fn();
-    console.log = jest.fn(); 
-    await service.insertBulkDimensionData(mockDimensionGrammar(), mockData);
-
-    expect(qbService.generateBulkInsertStatementOld).toHaveBeenCalledWith(mockDimensionGrammar().schema, mockData);
-    expect(prismaService.$queryRawUnsafe).toHaveBeenCalledWith(mockInsertQuery);
-    if (mockData.length < 50) {
-      expect(console.log).toHaveBeenCalledWith(mockData);
-    }
-    expect(console.error).toHaveBeenCalledWith(mockDimensionGrammar().name);
-    expect(console.error).toHaveBeenCalledWith(mockError);
-  });
-
-  it('should insert dimension data using correct insert query', async () => {
-    const dimensionGrammar: DimensionGrammar = {
-      name: 'test_dimension',
-      description: 'Test dimension',
-      type: 'dynamic',
-      storage: {
-        indexes: ['id'],
-        primaryId: 'id',
-        retention: null,
-        bucket_size: null,
-      },
-      schema: {
-        title: 'test_dimension',
-        psql_schema: 'dimensions',
-        properties: {
-          id: {
-            type: 'integer',
-            unique: true,
-          },
-          name: {
-            type: 'string',
-            unique: false,
-          },
-          description: {
-            type: 'string',
-            unique: false,
-          },
-        },
-        indexes: [],
-      },
-    };
-
+  it('calls generateBulkInsertStatementOld() function to return the mock insert query and verify insertion', async () => {
+    await service.prisma.$executeRawUnsafe(
+      `DROP TABLE IF EXISTS "dimensions"."school";`,
+    );
+    await service.createDimension(grammar, false);
     const data = [
-      { id: 1, name: 'Item 1', description: 'Description 1' },
-      { id: 2, name: 'Item 2', description: 'Description 2' },
-    ];
-    const mockInsertQuery = 'INSERT INTO test_dimension (id, name, description) VALUES (1, "Item 1", "Description 1"), (2, "Item 2", "Description 2")';
-    jest.spyOn(qbService, 'generateInsertStatement').mockReturnValue(mockInsertQuery);
-    prismaService.$queryRawUnsafe = jest.fn();
-    await service.insertDimensionData(dimensionGrammar, data);
-
-    expect(qbService.generateInsertStatement).toHaveBeenCalledWith(dimensionGrammar.schema, data);
-    expect(prismaService.$queryRawUnsafe).toHaveBeenCalledWith(mockInsertQuery);
-  });
-
+      { name: 'school1', date_created: '2020-01-01T00:00:00.000Z' },
+      { name: 'school2', date_created: '2020-01-02T00:00:00.000Z' },
+    ];    
+    await service.insertBulkDimensionDataV2(grammar, data);
+    const result = await service.prisma.$queryRawUnsafe(
+        `select * from dimensions.school;`,
+      );
+      expect(result[0].name).toEqual('school1');
+      expect(result[1].name).toEqual('school2');
+    await service.prisma.$executeRawUnsafe(
+        `DROP TABLE IF EXISTS "dimensions"."school";`,
+      );
+  });  
 });
